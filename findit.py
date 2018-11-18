e0a6bf45d4c97682503e5d1b44bb6febb2ca080b
jaythaceo <jaythaceo@gmail.com>
2018-11-18 16:30:34 -0500

    [chore] towers of hanoi

--- findit.py
+++ findit.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python3
 """
-Data testing NFL shit
-maybe help some gambling
-nuts I know or my fantasy
-WHO KNOWS!!
+Tower of Hanoi!
 """
 
-import pandas as pd # data manipulation library
-import numpy as np # numerical computation library
-import datetime as dt 
+def hanoi(n, p1, p2, p3):
+	# Move n discs to move this step
+	if n == 0:
+		return
+
+	global count
+	count += 1
+
+	# Move n-1 discs from p1 to p2
+	hanoi(n-1,p1,p3, p2)
+
+	if p1:
+		p3.append(p1.pop())
+		print(a,b,c)
+
+	hanoi(n-1, p2,p1,p3)
+
+n = 3
+a = list(range(n,0,-1))
+b, c = [], []
+
+print(a,b,c)
+count = 0
+hanoi(n, a,b,c)
+print(count)
