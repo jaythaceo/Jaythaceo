d77f9067c8f25888da812fe152f27526593763d7
jaythaceo <jaythaceo@gmail.com>
2022-03-13 12:40:05 -0400

    [misc] Added a bunch of useless files

--- jason.py
+++ jason.py
@@ -1,17 +1,15 @@
-#!/usr/bin/env python3
-
-"""
-fname = "romeo.txt"
-file = open(fname)
-lst  = list()
-for line in file.readlines():
-	print(line.rstrip())
-"""
+#!/usr/bin/env python
+import csv
 
 
-from random import randint
+data = open("airbus_tree.csv")
+csvreader = csv.reader(file)
+header = next(csvreader)
+print(header)
+rows = []
 
+for rows in csvreader:
+	rows.append(rows)
 
-for _ in range(1,10):
-	value = randint(1,10)
-	print(value)
+print(rows)
+file.close()
