aded5edf4a683f3f37ac27a74013c4bd202e55ed
jaythaceo <jaythaceo@gmail.com>
2025-01-11 14:30:24 -0500

    [chore] Playing with bubblesort

--- craps/sort.py
+++ craps/sort.py
@@ -2,6 +2,22 @@
 # Let's do some stuff here.
 # Now maybe a class is needed?
 
-a = [12,34,54,66,53]
-a.count(a)
-#print(a)
+def bubble_sort(arr):
+  """" Sorts a list using bubblesort """
+  n = len(arr)
+
+  for i in range(n):
+    for j in range(0, n - i - 1):
+      if arr[j] > arr[j +1]:
+        arr[j], arr[j +1] = arr[j +1], arr[j]
+
+
+  return arr
+ 
+
+
+
+my_list = [123,34,254,7866,53]
+print("Unsorted list:", my_list)
+sorted_list = bubble_sort(my_list)
+print("Sorted list:", sorted_list)
