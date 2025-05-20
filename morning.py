8ff70ef0d9e013a0c025d6ca534bdde43afe4f9e
jaythaceo <jaythaceo@gmail.com>
2025-05-19 23:29:54 -0400

    [chore] Added quick sort function

--- morning.py
+++ morning.py
@@ -1,5 +1,21 @@
 
 # Something to do this morning 
 
-def main():
-  print("hello")
+def quick_sort(sequence):
+  lesser =[]
+  equal = []
+  greater = []
+  if len(sequence) <= 1:
+    return sequence
+  pivot = sequence[0]
+  for element in sequence:
+    if element < pivot:
+      lesser.append(element)
+    elif element > pivot:
+      greater.append(element)
+    else:
+      equal.append(element)
+
+  lesser = quick_sort(lesser)
+  greater = quick_sort(greater)
+  return lesser + equal + greater
