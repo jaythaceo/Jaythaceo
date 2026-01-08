18856cff20d31941ef9c28551ce99008359e208a
jaythaceo <jaythaceo@gmail.com>
2026-01-08 11:32:40 -0500

    [chore] Added chaotic function

--- newVS.py
+++ newVS.py
@@ -1,8 +1,15 @@
 # Lets do something
+"""A simple chaotic function demonstration
+This function takes a number between 0 and 1 and applies the logistic map
+    repeatedly to demonstrate chaotic behavior.
+def main():
+    print("Lets do a chaotic function")
+    x = eval(input("Enter a number between 0 and 1: "))
+    for i in range(10):
+        x = 3.9 * x * (1 - x)
+        print(x)
+    
+main()
 
-print("Jason needs help!!!")
+"""
 
-helpme = "I am stuck in a code snippet!"
-print(helpme)
-print("Please send help ASAP!")
-print("Thank you!")
