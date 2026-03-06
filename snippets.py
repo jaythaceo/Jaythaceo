2b8ccdd434b85f860762ff230107e978cd0ecfde
jaythaceo <jaythaceo@gmail.com>
2026-03-06 12:38:00 -0500

    [chore] finished properties

--- snippets.py
+++ snippets.py
@@ -204,7 +204,28 @@ print(dog.speak())
 print(cat.speak())
 
 # Properties
-claas Circle:
+class Circle:
   def __init__(self, radius):
     self._radius = radius
-    
+  
+  @property
+  def radius(self):
+    return self._radius
+  
+  @radius.setter
+  def radius(self, value):
+    if value < 0:
+      raise ValueError("Radius cannot be negative.")
+    self._radius = value
+  
+  @property
+  def diameter(self):
+    return self.radius * 2
+  
+  @property
+  def area(self):
+    import math
+    return math.pi * (self.radius ** 2)
+
+
+
