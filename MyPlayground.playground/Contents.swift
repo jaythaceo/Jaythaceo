80eec2ef3b5104c5c326b1abee50b4a86f1cb6f5
jaythaceo <jaythaceo@gmail.com>
2026-03-17 18:27:07 -0400

    [chore] Adding Swift snippets

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,8 @@
 import UIKit
 
-var greeting = "Hello, playground"
-print(greeting)
+let temperature = 85
 
+if temperature < 55 {
+    print("It's cold outside")
+} else if temperature >= 55 && temp
+            
