4e107e8782e81d11b5a6cf3538f509a24562f969
jaythaceo <jaythaceo@gmail.com>
2026-04-25 15:14:41 -0400

    [chore] Added sum of difference range fix

--- testShit.py
+++ testShit.py
@@ -11,3 +11,13 @@ def sum_of_differences(lst):
         return sum
     
 print(sum_of_differences([1, 2, 10]))
+
+# def sum_of_differences(lst):
+#     if len(lst) < 2:
+#         return 0
+#     else:
+#         lst.sort()
+#         sum = 0
+#         for i in range(len(lst)-1):
+#             sum += lst[i+1] - lst[i]
+#         return sum
