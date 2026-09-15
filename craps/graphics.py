a6803883c2d220a77cce4a4a2834990df33715df
jaythaceo <jaythaceo@gmail.com>
2026-09-14 22:54:29 -0400

    [chore] added mail-in spec

--- craps/graphics.py
+++ craps/graphics.py
@@ -4,7 +4,7 @@
 The library is designed to make it very easy for novice programmers to
 experiment with computer graphics in an object oriented fashion. It is
 written by John Zelle for use with the book "Python Programming: An
-Introduction to Computer Science" (Franklin, Beedle & Associates).
+Introduction to Computer Science" (Franklin,Beedle & Associates).
 
 LICENSE: This is open-source software released under the terms of the
 GPL (http://www.gnu.org/licenses/gpl.html).
